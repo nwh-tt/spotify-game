7a1800127b5fc0e5a045b6dcbd36320754512f3b
Ned_Whitt <nedwhitt@vt.edu>
2022-07-16 18:39:36 -0400

    Got the api functioning. No funcitonality yet

--- src/App.js
+++ src/App.js
@@ -1,38 +1,37 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import { Grid, Box, Divider, Typography } from "@mui/material";
+import { Grid, Box, Divider, Typography, CssBaseline } from "@mui/material";
+import { createTheme, ThemeProvider } from "@mui/material/styles";
 import SongPanel from "./SongPanel";
 
-function createTrackObject() {
-  return {
-    albumImage: "",
-    artists: "",
-    name: "",
-    popularity: 0,
-  };
-}
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+  typography: {
+    fontFamily: ["Poppins", "Roboto", "Helvitica", "sans-serif", "Arial"].join(
+      ","
+    ),
+  },
+});
 
 function processData(data) {
-  /*
   const song = {
     albumImage: data.album.images[0].url,
     artist: data.artists[0].name,
     name: data.name,
     popularity: data.popularity,
   };
-  return song; */
-  return {
-    albumImage:
-      "https://i.scdn.co/image/ab67616d0000b273b46f74097655d7f353caab14",
-    artist: "Harry Styles",
-    name: "As It Was",
-    popularity: 98,
-  };
+  return song;
 }
+// gets random int between 0 and 99
+const getRandomNumber = () => {
+  return Math.floor(Math.random() * 100);
+};
 
 function App() {
-  const [playlist, setPlaylist] = useState([{ track: "" }]);
-  /*
+  const [playlist, setPlaylist] = useState([]);
+  const [loaded, setLoaded] = useState(false);
   useEffect(() => {
     const options = {
       method: "GET",
@@ -48,46 +47,58 @@ function App() {
       .then((response) => response.json())
       .then((data) => {
         setPlaylist(data.tracks.items);
+        setLoaded(true);
       })
       .catch((err) => console.error(err));
   }, []);
-  */
-  // console.log(playlist);
+
   const styles = {
-    test: {
-      backgroundColor: "#f5f5f5",
-      margin: "0px",
-      padding: "0px",
-      overflow: "hidden",
-    },
     outline: {
-      border: "5px solid #e0e0e0",
-      height: "98.5vh",
-      margin: "0px",
-      padding: "0px",
+      height: "100vh",
     },
   };
 
   return (
-    <Box sx={styles.test}>
-      <Grid
-        container
-        direction="row"
-        alignItems="center"
-        justifyContent="space-around"
-        sx={styles.outline}
-      >
-        <Grid item xs>
-          <SongPanel song={processData(playlist[0].track)} />
-        </Grid>
-        <Divider orientation="vertical">
-          <Typography>vs</Typography>
-        </Divider>
-        <Grid item xs>
-          <SongPanel song={processData(playlist[0].track)} />
-        </Grid>
-      </Grid>
-    </Box>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      {loaded ? (
+        <Box>
+          <Grid
+            container
+            direction="row"
+            alignItems="center"
+            justifyContent="space-around"
+            sx={styles.outline}
+          >
+            <Grid item xs>
+              <SongPanel
+                song={processData(playlist[getRandomNumber()].track, loaded)}
+                left={true}
+              />
+            </Grid>
+            <Divider orientation="vertical">
+              <Typography
+                sx={{
+                  fontSize: "40px",
+                  marginBottom: "100px",
+                  paddingTop: "5px",
+                }}
+              >
+                VS
+              </Typography>
+            </Divider>
+            <Grid item xs>
+              <SongPanel
+                song={processData(playlist[getRandomNumber()].track, loaded)}
+                left={false}
+              />
+            </Grid>
+          </Grid>
+        </Box>
+      ) : (
+        <Typography>Loading...</Typography>
+      )}
+    </ThemeProvider>
   );
 }
 
--- src/SongPanel.js
+++ src/SongPanel.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { Typography, Box } from "@mui/material";
+import { Typography, Box, Button } from "@mui/material";
 // create a song object
 
 export default function SongPanel(props) {
   // vertically center the song name
   const styles = {
     songName: {
-      // border: "1px solid #e0e0e0",
       // borderRadius: "5px",
       textAlign: "center",
-      paddingBottom: "20px",
+      paddingBottom: "20vh",
+      height: "70vh",
     },
     text: {
-      fontSize: "30px",
+      fontSize: "40px",
+      margin: "10px",
+    },
+    button: {
       margin: "10px",
     },
   };
@@ -22,6 +25,31 @@ export default function SongPanel(props) {
     <Box sx={styles.songName}>
       <img src={props.song.albumImage} alt="error" />
       <Typography sx={styles.text}>{props.song.name}</Typography>
+      <Typography sx={{ fontSize: "12px" }}>
+        {props.left ? "has" : "has a"}
+      </Typography>
+      {props.left ? (
+        <Typography sx={styles.text}>{props.song.popularity}</Typography>
+      ) : (
+        <Box>
+          <Button
+            variant="contained"
+            color="primary"
+            size="large"
+            sx={styles.button}
+          >
+            Higher
+          </Button>
+          <Button
+            variant="contained"
+            color="error"
+            size="large"
+            sx={styles.button}
+          >
+            Lower
+          </Button>
+        </Box>
+      )}
     </Box>
   );
 }
